78de6a21d0898ff389afdadcad2cb8a3dfbfa0be
Vini Pereira <viniciusxmp@gmail.com>
2023-01-25 16:52:15 -0300

    Create functions to run the basic operations

---
+++ app.js
@@ -0,0 +1,19 @@
+const add = function(num1, num2) {
+    return num1 + num2;
+}
+
+const subtract = function(num1, num2) {
+    return num1 - num2;
+}
+
+const multiply = function(num1, num2) {
+    return num1 * num2;
+}
+
+const divide = function(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
