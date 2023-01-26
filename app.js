77851cff569cd88ac6bfe8f4d749d7872302b814
Vini Pereira <viniciusxmp@gmail.com>
2023-01-26 20:22:25 -0300

    Populate display content when buttons are clicked

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const displayElement = document.querySelector('.display');
+
+const digitButtons = document.querySelectorAll('button.digit');
+
+const clearButton = document.querySelector(`[data-function='clear']`);
+
+let displayContent = '';
+
 const add = function(num1, num2) {
     return num1 + num2;
 }
@@ -17,3 +25,19 @@ const divide = function(num1, num2) {
 function operate(operator, num1, num2) {
     return operator(num1, num2);
 }
+
+function display(e) {
+    displayContent += e.target.textContent;
+    displayElement.textContent = displayContent;
+}
+
+function clear() {
+    displayContent = '';
+    displayElement.textContent = displayContent;
+}
+
+digitButtons.forEach((digitButton) => {
+    digitButton.addEventListener('click', display);
+});
+
+clearButton.addEventListener('click', clear);
