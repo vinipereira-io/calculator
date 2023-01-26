c705c325014f7f21a3d216c08fcf2dda7e6dbee9
Vini Pereira <viniciusxmp@gmail.com>
2023-01-26 20:54:16 -0300

    Store first number and operation

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const displayElement = document.querySelector('.display');
 
-const digitButtons = document.querySelectorAll('button.digit');
+const digitButtons = document.querySelectorAll('.digit');
 
-const clearButton = document.querySelector(`[data-function='clear']`);
+const operatorButtons = document.querySelectorAll('.operator');
 
-let displayContent = '';
+const clearButton = document.querySelector('.clear');
+
+const equalsButton = document.querySelector('.equals');
+
+let displayContent = '',
+    num1,
+    num2,
+    operation;
 
 const add = function(num1, num2) {
     return num1 + num2;
@@ -26,8 +33,9 @@ function operate(operator, num1, num2) {
     return operator(num1, num2);
 }
 
-function display(e) {
-    displayContent += e.target.textContent;
+function display(content) {
+    if (!content) displayContent = '';
+    displayContent += content;
     displayElement.textContent = displayContent;
 }
 
@@ -36,8 +44,19 @@ function clear() {
     displayElement.textContent = displayContent;
 }
 
+function input(numInput, operationInput) {
+    num1 = numInput;
+    operation = operationInput;
+    display('');
+    console.log(num1, operationInput);
+}
+
 digitButtons.forEach((digitButton) => {
-    digitButton.addEventListener('click', display);
+    digitButton.addEventListener('click', () => {display(digitButton.textContent);});
 });
 
 clearButton.addEventListener('click', clear);
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', () => {input(displayContent, operatorButton.dataset.operation);});
+});
